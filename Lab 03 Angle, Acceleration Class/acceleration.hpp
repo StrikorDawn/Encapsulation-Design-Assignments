36087321d341cf0b6e90afa8cdce59e6daba5935
Mark Van Horn <60052863+krammark191@users.noreply.github.com>
2025-01-22 11:43:28 -0700

    Update acceleration.hpp
    sdfgjksdflvk

--- Lab 03 Angle, Acceleration Class/acceleration.hpp
+++ Lab 03 Angle, Acceleration Class/acceleration.hpp
@@ -1,13 +1,36 @@
-//
-//  acceleration.hpp
-//  Lab 03 Angle, Acceleration Class
-//
-//  Created by Mark Van Horn on 1/22/25.
-//
+/*************************************************************
+ * 1. Name:
+ *      Mark Van Horn & Taden Marston
+ * 2. Assignment Name:
+ *      Lab 03: Apollo 11 Angle, Acceleration Class
+ * 3. Assignment Description:
+ *      A class to represent an angle
+ * 4. What was the hardest part? Be as specific as possible.
+ *      This one was a learning process for us. Once we got the
+ *      program to compile we got a ton of failed tests, but after
+ *      an extremely helpful consultation we began the debugging
+ *      process which solved 89.7% of our problems, we then got
+ *      very stuck on the display portion, which was highly
+ *      frustrating because while the output was exactly what was
+ *      listed in the tests, the tests failed.
+ * 5. How long did it take for you to complete the assignment?
+ *      2.5 hours :(
+ **************************************************************/
+
 
 #ifndef acceleration_hpp
 #define acceleration_hpp
+#define TWO_PI 6.28318530718
 
 #include <stdio.h>
+#include <math.h>    // for floor()
+#include <iostream>  // for cout
+#include <cassert>   // for assert()
+
+using namespace std;
+
+class Angle;
+
+class Acceleration;
 
 #endif /* acceleration_hpp */
