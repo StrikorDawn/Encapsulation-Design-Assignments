b12c46326e7c3c17918489bec815d51f2eba6192
Mark Van Horn <60052863+krammark191@users.noreply.github.com>
2025-01-22 11:34:06 -0700

    Add files via upload
    Uploaded empty .cpp and .hpp files

---
+++ Lab 03 Angle, Acceleration Class/acceleration.cpp
@@ -0,0 +1,8 @@
+//
+//  acceleration.cpp
+//  Lab 03 Angle, Acceleration Class
+//
+//  Created by Mark Van Horn on 1/22/25.
+//
+
+#include "acceleration.hpp"
---
+++ Lab 03 Angle, Acceleration Class/acceleration.hpp
@@ -0,0 +1,13 @@
+//
+//  acceleration.hpp
+//  Lab 03 Angle, Acceleration Class
+//
+//  Created by Mark Van Horn on 1/22/25.
+//
+
+#ifndef acceleration_hpp
+#define acceleration_hpp
+
+#include <stdio.h>
+
+#endif /* acceleration_hpp */
