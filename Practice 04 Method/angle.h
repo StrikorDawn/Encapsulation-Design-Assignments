10ed705ad7bb12efec24dd34db834d7c466a85ef
Taden Marston <tadenmarston@gmail.com>
2025-01-28 14:08:15 -0700

    copied angle.h to practice 04.

--- Practice 04 Method/angle.h
+++ Practice 04 Method/angle.h
@@ -1,22 +1,30 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Mark Van Horn & Taden Marston
  * 2. Assignment Name:
- *      Practice 04: Angle Class
+ *      Practice 03: Angle Class
  * 3. Assignment Description:
  *      A class to represent an angle
  * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
+ *      This one was a learning process for us. Once we got the
+ *      program to compile we got a ton of failed tests, but after
+ *      an extremely helpful consultation we began the debugging
+ *      process which solved 89.7% of our problems, we then got
+ *      very stuck on the display portion, which was highly
+ *      frustrating because while the output was exactly what was
+ *      listed in the tests, the tests failed.
  * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, etc.
+ *      2.5 hours :(
  **************************************************************/
 
+#pragma once
 
 #define TWO_PI 6.28318530718
 
 #include <math.h>    // for floor()
 #include <iostream>  // for cout
 #include <cassert>   // for assert()
+#include <cmath>     // for fmod()
 using namespace std;
 
 class TestAngle;
@@ -27,32 +35,79 @@ class TestAngle;
 class Angle
 {
    friend TestAngle;
-
-public:
-   // Default constructor
-   Angle()                   { }
-   Angle(const Angle & rhs)  { }
-   Angle(double degrees)     { }
-
-   // Getters
-   double getDegrees() const { return -1.0; }
-   double getRadians() const { return -1.0; }
-
-   // Setters
-   void setDegrees(double degrees) { }
-   void setRadians(double radians) { }
-
-   // Display
-   void display(ostream & out) const { }
-
-private:
-   // Convert functions
-   double convertToDegrees(double radians) const { return -1.0; }
-   double convertToRadians(double degrees) const { return -1.0; }
-
-   // Normalize
-   double normalize(double radians) const { return  -1.0; }
-
-   double radians;
+   private:
+   
+   /***************************************
+    * CONVERT
+    ***************************************/
+   
+      // normalizes radians to 0 < radian < TWO_PI
+      double normalize(double radian)
+      {
+         radian = fmod(radian, TWO_PI);
+         if (radian < 0)
+            radian += TWO_PI;
+         return radian;
+      }
+   
+      // normalizes radians and then converts to degrees
+      double convertToDegrees(double radian)
+      {
+         return normalize(radian) * (360 / TWO_PI);
+      }
+   
+      // converts to radians and then normalizes
+      double convertToRadians(double degrees)
+      {
+         return normalize(degrees * (TWO_PI / 360));
+      }
+   public:
+      double radians = 0;
+   
+   /***************************************
+   * SET
+   ***************************************/
+   
+      // converts radians to degrees and returns the value
+      double getDegrees()
+      {
+         return convertToDegrees(radians);
+      }
+   
+      // returns radians
+      double getRadians()
+      {
+         return radians;
+      }
+   
+   /***************************************
+   * SET
+   ***************************************/
+   
+      // converts degrees param to radians and sets value
+      void setDegrees(double degrees)
+      {
+         radians = convertToRadians(degrees);
+      }
+   
+      // sets radians to param value
+      void setRadians(double radian)
+      {
+         radians = normalize(radian);
+      }
+   
+   /***************************************
+    * DISPLAY
+    ***************************************/
+   
+      //Displays radians variable in degrees format
+      ostream& display(ostream& out)
+      {
+         out.setf(ios::fixed);     // "fixed" means don't use scientific notation
+         out.setf(ios::showpoint); // "showpoint" means always show the decimal point
+         out.precision(1);         // Set the precision to 1 decimal place of accuracy.
+         out << convertToDegrees(radians) << "degrees";
+         return out;
+      }
 };
 
