f09455af2f70d221765c0dbea92ab96bb091aa21
Mark Van Horn <60052863+krammark191@users.noreply.github.com>
2025-01-30 14:57:09 -0700

    Update position.cpp

--- Lab 04 Apollo 11 Position Velocity/position.cpp
+++ Lab 04 Apollo 11 Position Velocity/position.cpp
@@ -15,8 +15,10 @@
  * POINT : CONSTRUCTOR WITH X,Y
  * Initialize the point to the passed position
  *****************************************/
-Position::Position(double x, double y) : x(99.9), y(88.8)
+Position::Position(double x, double y)
 {
+   this->x = x;
+   this->y = y;
 }
 
 /******************************************
@@ -26,7 +28,7 @@ Position::Position(double x, double y) : x(99.9), y(88.8)
  *****************************************/
 void Position::add(const Acceleration & a, const Velocity & v, double t)
 {
-   x = 99.9;
-   y = 88.8;
+   x += v.getDX() * t + 0.5 * a.getDDX() * t * t;
+   y += v.getDY() * t + 0.5 * a.getDDY() * t * t;
 }
 
