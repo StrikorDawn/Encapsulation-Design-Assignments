8afa0647a31e2d0536de26652eaabe62bf2a9256
Mark Van Horn <60052863+krammark191@users.noreply.github.com>
2025-02-18 14:01:23 -0700

    Add files via upload

--- W07 Practice/wallet.h
+++ W07 Practice/wallet.h
@@ -1,6 +1,6 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Mark Van Horn & Taden Marston
  * 2. Module
  *      WALLET
  * 3. Assignment Description:
@@ -13,12 +13,47 @@
 
 class TestWallet;
 
- /************************************
-  * WALLET
-  ************************************/
+/************************************
+ * WALLET
+ ************************************/
 class Wallet
 {
    friend TestWallet;
-
+   
+   private:
+   double accountSam;
+   double accountSue;
+   double* pCurrentAccount;
+   
+   public:
+   // Constructor
+   Wallet() : accountSam(0.0), accountSue(0.0), pCurrentAccount(&accountSam) {}
+   
+   // Update methods
+   void updateSam(double amount) { accountSam += amount; }
+   void updateSue(double amount) { accountSue += amount; }
+   
+   void updateCurrent(double amount)
+   {
+      if (pCurrentAccount)
+         *pCurrentAccount += amount;
+   }
+   
+   // Choose methods
+   void chooseSam() { pCurrentAccount = &accountSam; }
+   void chooseSue() { pCurrentAccount = &accountSue; }
+   
+   void chooseCurrent()
+   {
+      pCurrentAccount = (accountSam > accountSue) ? &accountSam : &accountSue;
+   }
+   
+   // Get methods
+   double getSam() const { return accountSam; }
+   double getSue() const { return accountSue; }
+   
+   double getCurrent() const
+   {
+      return pCurrentAccount ? *pCurrentAccount : 0.0;
+   }
 };
-
