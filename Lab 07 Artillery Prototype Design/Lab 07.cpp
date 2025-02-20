2ed2dcbbe0986dbc5a4c8dc10f6f1e8c6123eb1b
Taden Marston <tadenmarston@gmail.com>
2025-02-20 15:33:40 -0700

    Added some variables

---
+++ Lab 07 Artillery Prototype Design/Lab 07.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+double acceleration;
+double drag;
+
+double angle;
+double dx;
+double dy;
+double position;
+double altitude;
+double velocity;
+double gravity;
+
+
+
+int main() {
+	for (int i = 0; i < 20; i++)
+	{
+		cout << i;
+	}
+
+}
