85ec49ad6f5d1bb553e748cd3d7e3ad0b033da99
Mark Van Horn <krammark191@gmail.com>
2025-02-20 19:04:34 -0700

    Added variable gravity and interpolation

--- Lab 07 Artillery Prototype Design/Lab 07.cpp
+++ Lab 07 Artillery Prototype Design/Lab 07.cpp
@@ -5,20 +5,48 @@ using namespace std;
 
 const double PI = 3.141592653589793;
 const double INITIAL_SPEED = 827.0; // m/s
-const double ANGLE_DEGREES = 75.0;
-const double T = 0.01;
+const double ANGLE_DEGREES = 75.0; // degrees
+const double T = 0.01; // seconds
 const double G = -9.8;
+const int ARR_SIZE = 20;
+// Array of gravity values based on altitude
+const double G_ARRAY[] = {9.807, 9.804, 9.801, 9.797, 9.794, 9.791, 9.788,
+                          9.785, 9.782, 9.779, 9.776, 9.761, 9.745, 9.730,
+                          9.715, 9.684, 9.654, 9.624, 9.594, 9.564};
+// Array of altitude variables
+const double A_ARRAY[] = {0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000,
+                          9000, 10000, 15000, 20000, 25000, 30000, 40000,
+                          50000, 60000, 70000, 80000};
 
-double acceleration;
-double drag;
 double dx;  // horizontal velocity
 double dy;  // vertical velocity
 double x;   // latitude
 double y;   // altitude
 double ht;  // Hang time
+double g;   // gravity
 
+double interpolate(double d0, double d1, double r0, double r1, double d)
+{
+   double r;
+   r = (r1 * d1 - r1 * d0 - r0 * d1 + r0 * d0 + r0 * d0) / (d - d0);
+   return r;
+}
 
-
+double findGravity(double altitude)
+{
+   for (int i = 0; i < ARR_SIZE; i++)
+   {
+      if (altitude == A_ARRAY[i])
+         return G_ARRAY[i];
+      else if (altitude > A_ARRAY[i] && altitude < A_ARRAY[i + 1])
+         return -interpolate(A_ARRAY[i],
+                             A_ARRAY[i + 1],
+                             G_ARRAY[i],
+                             G_ARRAY[i + 1],
+                             altitude);
+   }
+   return 0.0;
+}
 
 int main()
 {
@@ -30,19 +58,23 @@ int main()
    double dy = INITIAL_SPEED * cos(angle_radians);
    
    // Initialize position
-   double x = 0.0;
-   double y = 0.0;
+   x = 0.0;
+   y = 0.0;
+   ht = 0.0;
+   g = 0.0;
    
    // Loop through 20 time units
    while (y >= 0)
    {
+      g = findGravity(y);
+      
       // Time is 1 because t changes from 1-20
       x += dx * T + 0.5 * 0 * T * T;
-      y += dy * T + 0.5 * G * T * T;
+      y += dy * T + 0.5 * g * T * T;
       
       // Update the velocity
-      dx += 0 * 1;
-      dy += G * 1;
+      dx += 0 * T;
+      dy += g * T;
       
       // Update the hang time
       ht += T;
