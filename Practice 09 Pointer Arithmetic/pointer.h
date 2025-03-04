5d5fb6da3bfc52bc3d090261869b95a456b9e275
Taden Marston <tadenmarston@gmail.com>
2025-03-04 12:58:18 -0700

    fixed python bleed

--- Practice 09 Pointer Arithmetic/pointer.h
+++ Practice 09 Pointer Arithmetic/pointer.h
@@ -1,10 +1,6 @@
 /*************************************************************
  * 1. Name:
-<<<<<<< HEAD
- *      Taden Marston & Mark Van Horn
-=======
  *      Mark Van Horn & Taden Marston
->>>>>>> 6540bca72f0b2aa86e891f690b737151608200fa
  * 2. Assignment Name:
  *      Practice 09: Pointer Arithmetic
  * 3. Assignment Description:
@@ -21,13 +17,13 @@
  **************************************/
 int countArray(char text[], char letter)
 {
-	int letterCount;
+	int letterCount = 0;
 
 	for (int i = 0; i < strlen(text); i++)
 	{
 		if (text[i] == letter)
 		{
-			letterCount += 1;
+			letterCount++;
 		}
 	}
 	return letterCount;
