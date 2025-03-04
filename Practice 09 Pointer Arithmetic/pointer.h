5c68bcc1d4f57726410d2676765ba96126cedb15
Mark Van Horn <krammark191@gmail.com>
2025-03-04 13:04:09 -0700

    Did stuff

--- Practice 09 Pointer Arithmetic/pointer.h
+++ Practice 09 Pointer Arithmetic/pointer.h
@@ -6,28 +6,26 @@
  * 3. Assignment Description:
  *      Traverse a string using pointer notation
  * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
+ *      The hardest part was figuring out exactly how to write
+ *      the countPointer function, but a quick review of the reading
+ *      solved that problem very easily.
  * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, etc.
+ *      30 minutes.
  **************************************************************/
 
 
- /**************************************
-  * COUNT - ARRAY
-  **************************************/
+/**************************************
+ * COUNT - ARRAY
+ **************************************/
 int countArray(char text[], char letter)
 {
-	int letterCount = 0;
-	int length = strlen(text);
-
-	for (int i = 0; i < length; i++)
-	{
-		if (text[i] == letter)
-		{
-			letterCount++;
-		}
-	}
-	return letterCount;
+   int count = 0;
+   for (int i = 0; i < strlen(text); i++)
+   {
+      if (text[i] == letter)
+         count++;
+   }
+   return count;
 }
 
 /**************************************
@@ -35,11 +33,11 @@ int countArray(char text[], char letter)
  **************************************/
 int countPointer(char text[], char letter)
 {
-	int count = 0;
-	for (char* p = text; *p; p++)
-	{
-		if (*p == letter)
-			count++;
-	}
-	return count;
+   int count = 0;
+   for (char* p = text; *p; p++)
+   {
+      if (*p == letter)
+         count++;
+   }
+   return count;
 }
